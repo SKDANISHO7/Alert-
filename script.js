6f1623024fd065f00942f2f4664ef720b7159ffb
SKDANISHO7 <138429874+SKDANISHO7@users.noreply.github.com>
2024-11-04 20:50:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,4 +2,4 @@
 let name = prompt("What is your name?");
 
 // Display a greeting message
-alert("Hello, " + name + " Welcome to Shaikh Danish's Heart.");
+alert("Hello, " + name + " Welcome to Shaikh Danish's ERA.");
